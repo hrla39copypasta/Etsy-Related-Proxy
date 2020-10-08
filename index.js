f03caf9c0d2f4a5da58d8828c174c0786503b4e8
Justin Murakami <jmurakami84@gmail.com>
2020-10-07 19:33:03 -0700

    Updated to handle the main product display module.

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
 const port = 3000;
 const host = 'localhost';
-const apiServiceUrl = 'http://localhost:8005';
+
+app.use(express.static(path.join(__dirname, './public')));
 
 app.use(morgan('dev'));
 
-app.use('/', createProxyMiddleware({
-  target: apiServiceUrl,
+app.use('/user', createProxyMiddleware({
+  target: 'http://localhost:8001',
+  changeOrigin: true
+}));
+app.use('/entry', createProxyMiddleware({
+  target: 'http://localhost:8001',
+  changeOrigin: true
+}));
+app.use('/product-display', createProxyMiddleware({
+  target: 'http://localhost:8002',
+  changeOrigin: true
+}));
+app.use('/photo-display', createProxyMiddleware({
+  target: 'http://localhost:8002',
+  changeOrigin: true
+}));
+app.use('/reviewsItem', createProxyMiddleware({
+  target: 'http://localhost:8003',
+  changeOrigin: true
+}));
+app.use('/reviewsShop', createProxyMiddleware({
+  target: 'http://localhost:8003',
+  changeOrigin: true
+}));
+app.use('/api', createProxyMiddleware({
+  target: 'http://localhost:8004',
+  changeOrigin: true
+}));
+app.use('/related', createProxyMiddleware({
+  target: 'http://localhost:8005',
   changeOrigin: true
 }));
 
